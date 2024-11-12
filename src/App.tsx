05df98ff5ce04cd4912be45c042ae5fc417775aa
Camilla <chri.camilla@gmail.com>
2024-11-12 12:09:22 +0100

    lagt til rute for romoversikt

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { Header } from "./components/layout/Header";
 import { LoginPage } from "./pages/auth/LoginPage";
 import { ProtectedRoute } from "./components/auth/ProtectedRoute";
+import RoomOverviewPage from "./pages/roomOverviewPage";
 
 function App() {
-  return <>
-  <AuthProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route element={<ProtectedRoute />}>
-            {/* Protected routes will go here */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  </>;
+  return (
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/rooms" element={<RoomOverviewPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route element={<ProtectedRoute />}>
+              {/* Protected routes will go here */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  );
 }
 
 export default App;
