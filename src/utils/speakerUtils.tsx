17ae742287c4b86147b667e66d1d047912f90cdc
Eduardo <cojr.eduardo@gmail.com>
2024-11-12 15:29:39 +0100

    created a new file called speakerUtils.tsx

---
+++ src/utils/speakerUtils.tsx
@@ -0,0 +1,16 @@
+import { getSpeakers, createSpeaker } from "../services/speakerService";
+
+export const createSpeakers = async () => {
+  const existingSpeakers = await getSpeakers();
+
+  if (existingSpeakers.items.length === 0) {
+    const defaultSpeakers = [
+      { name: "Eduardo Jr Co", biography: "Test Biography" },
+      { name: "Eduardo Jr Co", biography: "Test Biography" },
+    ];
+
+    for (const speaker of defaultSpeakers) {
+      await createSpeaker(speaker);
+    }
+  }
+};
