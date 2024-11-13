d8a76fa3218ac0f2577d73608eade2c831d75ac2
Eduardo <cojr.eduardo@gmail.com>
2024-11-13 12:59:38 +0100

    added function for the chosen speaker and a function to return to the overview page.

--- src/pages/SpeakerOverviewPage.tsx
+++ src/pages/SpeakerOverviewPage.tsx
@@ -1,10 +1,12 @@
-import { getSpeakers } from "../services/speakerService";
+import { getSpeakers, getSpecificSpeaker } from "../services/speakerService";
 import React, { useEffect, useState } from "react";
 import "../App.css";
 
 const SpeakerOverviewPage = () => {
   const [speakers, setSpeakers] = useState([]);
+  const [chosenSpeaker, setChosenSpeaker] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [loadingMoreDetails, setLoadingMoreDetails] = useState(false);
 
   useEffect(() => {
     const fetchSpeakers = async () => {
@@ -15,6 +17,18 @@ const SpeakerOverviewPage = () => {
     fetchSpeakers();
   }, []);
 
+  const clickChosenSpeaker = async (id) => {
+    setLoadingMoreDetails(true);
+    const speakerData = await getSpecificSpeaker(id);
+    setChosenSpeaker(speakerData);
+    setLoadingMoreDetails(false);
+  };
+
+  //Clicking back to speaker overview
+  const clickBack = () => {
+    setChosenSpeaker(null);
+  };
+
   useEffect(() => {
     console.log("Speakers: ", speakers);
   }, [speakers]);
@@ -24,10 +38,27 @@ const SpeakerOverviewPage = () => {
       <h1>Speaker Overview</h1>
       {loading ? (
         <p>Loading...</p>
+      ) : chosenSpeaker ? (
+        <div>
+          {loadingMoreDetails ? (
+            <p>Loading more details..</p>
+          ) : (
+            <div className="speaker-detail">
+              <h2>{chosenSpeaker.name}</h2>
+              <p>biography: {chosenSpeaker.biography}</p>
+              <button onClick={clickBack}>Back</button>
+            </div>
+          )}
+        </div>
       ) : (
         <div className="speaker-container">
           {speakers.map((speaker) => (
-            <div key={speaker._uuid} className="speaker-list">
+            <div
+              key={speaker._uuid}
+              className="speaker-list"
+              onClick={() => clickChosenSpeaker(speaker._uuid)}
+              style={{ cursor: "pointer" }}
+            >
               <h2>{speaker.name}</h2>
               <p>biography: {speaker.biography}</p>
             </div>
