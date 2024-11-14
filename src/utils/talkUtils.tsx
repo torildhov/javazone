e087102b133d9a84b21d24fd7de17ef11be94ab5
Elisabeth1690 <90ebaa@gmail.com>
2024-11-14 23:40:05 +0100

    gjort litt forskjellig og klarer å poste

--- src/utils/talkUtils.tsx
+++ src/utils/talkUtils.tsx
@@ -1,25 +1,35 @@
 import { getTalks, createTalk } from "../services/TalksService";
 
 export const createInitialTalks = async () => {
+  try {
   const existingTalks = await getTalks();
 
-  if (existingTalks.length === 0) {
+  if (existingTalks.length === 1) {
     const defaultTalks = [
       {
         title: "HTML",
         speaker: "Thomas Eriksson",
-        time: "20:00",
+        time: "20:00"
         
       },
       {
         title: "CSS",
         speaker: "Camilla Lackberg",
-        time: "12:00",
+        time: "12:00"
         
       }];
       
       for(const talk of defaultTalks){
-        await createTalk(talk);
-      } }};
+        const result = await createTalk(talk);
+        if(result){
+          console.log(`Created talk: ${talk.title}`);
+        }
+      }
+     }
+    }catch (error) {
+      console.error("Error in createInitialTalks:", error);
+ } 
+
+};
 
     
