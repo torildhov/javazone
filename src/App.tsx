b108aac81a48ce93365eb30396499b39e2237a09
Camilla <chri.camilla@gmail.com>
2024-11-16 15:22:55 +0100

    fjernet createInitialRooms

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { AuthProvider } from "./context/AuthContext";
 import { Header } from "./components/layout/Header";
 import { Footer } from "./components/layout/Footer";
 import { useEffect } from "react";
-import { createInitialRooms } from "./utils/roomUtils";
 import { createInitialSpeakers } from "./utils/speakerUtils";
 import { DataProvider } from "./context/DataContext";
 import { AppRoutes } from "./routes/Routes";
@@ -14,7 +13,6 @@ import { CustomCursor } from "./components/CustomCursor";
 
 function App() {
   useEffect(() => {
-    createInitialRooms();
     createInitialSpeakers();
   }, []);
   return (
