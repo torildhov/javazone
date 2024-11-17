3d8aa7b99b39d5ee7ef5b08c64834bab3c60caf6
Eduardo <cojr.eduardo@gmail.com>
2024-11-17 09:50:09 +0100

    created a new file for adding a new speaker

---
+++ src/components/speakers/AddSpeaker.tsx
@@ -0,0 +1,71 @@
+import { useContext, useEffect, useState } from "react";
+import { createSpeaker } from "../../services/speakerService";
+import { DataContext } from "../../context/DataContext";
+import { fetchAndSetSpeakers } from "../../utils/speakerUtils";
+
+const AddSpeaker: React.FC = () => {
+  const [newSpeakerName, setNewSpeakerName] = useState<string>("");
+  const [newSpeakerBiography, setNewSpeakerBiography] = useState<string>("");
+
+  const context = useContext(DataContext);
+  if (!context) {
+    throw new Error("Unable to find DataConext");
+  }
+
+  const { speakers, setSpeakers } = context;
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (newSpeakerName.trim() && newSpeakerBiography.trim()) {
+      const newSpeaker = {
+        name: newSpeakerName,
+        biography: newSpeakerBiography,
+      };
+      try {
+        await createSpeaker(newSpeaker);
+        await fetchAndSetSpeakers(setSpeakers);
+
+        setNewSpeakerName("");
+        setNewSpeakerBiography("");
+      } catch (error) {
+        console.error("Unable to add speaker", error);
+      }
+    } else {
+      console.log("All fields must be filled out!");
+    }
+  };
+
+  useEffect(() => {
+    console.log(speakers);
+  }, [speakers]);
+
+  return (
+    <div>
+      <h2>Add Speaker</h2>
+      <form action="">
+        <input
+          type="text"
+          placeholder="Speaker name"
+          value={newSpeakerName}
+          onChange={(e) => setNewSpeakerName(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Biography"
+          value={newSpeakerBiography}
+          onChange={(e) => {
+            setNewSpeakerBiography(e.target.value);
+          }}
+        />
+
+        <button type="submit" onClick={(e) => handleSubmit(e)}>
+          Add new speaker
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddSpeaker;
