ee85a2545f81733ef7a287bf88448b263aea6549
Elisabeth1690 <90ebaa@gmail.com>
2024-11-18 00:11:58 +0100

    håper på det beste

--- src/pages/TalksDetailePage.tsx
+++ src/pages/TalksDetailePage.tsx
@@ -80,56 +80,7 @@ if(newTilte && newRoomId && newSpeakerId && newTime){
   } catch (err) {
     console.error("Failed to update talk:", err);
   }
-
-  return (
-    <div className="edit-talk-form">
-      <h1>Edit Talk</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="speakerId">Speaker ID:</label>
-          <input
-            type="text"
-            id="speakerId"
-            name="speakerId"
-            value={formData.speakerId}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="roomId">Room ID:</label>
-          <input
-            type="text"
-            id="roomId"
-            name="roomId"
-            value={formData.roomId}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="time">Time:</label>
-          <input
-            type="text"
-            id="time"
-            name="time"
-            value={formData.time}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Save</button>
-        <button type="button" onClick={handleDelete}>Delete</button>
-      </form>
-    </div>
-  );
+}
 
 };
 
