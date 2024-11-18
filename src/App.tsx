936c4ee5e7de877f224872df839defd38235dd1d
Camilla <chri.camilla@gmail.com>
2024-11-18 12:44:54 +0100

    fjernet createInitialSPeakers, den trengs ikke

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { Header } from "./components/layout/Header";
 import { Footer } from "./components/layout/Footer";
-import { useEffect } from "react";
-import { createInitialSpeakers } from "./utils/speakerUtils";
 import { DataProvider } from "./context/DataContext";
 import { AppRoutes } from "./routes/Routes";
 import NeatBackground from "./components/layout/AnimatedBackground";
 import { CustomCursor } from "./components/layout/CustomCursor";
 
 function App() {
-  useEffect(() => {
-    createInitialSpeakers();
-  }, []);
   return (
     <>
       <AuthProvider>
--- src/utils/speakerUtils.tsx
+++ src/utils/speakerUtils.tsx
@@ -1,24 +1,5 @@
-import { getSpeakers, createSpeaker } from "../services/speakerService";
+import { getSpeakers } from "../services/speakerService";
 import { Speaker } from "../context/DataContext";
-
-export const createInitialSpeakers = async () => {
-  const existingSpeakers = await getSpeakers();
-
-  if (existingSpeakers.items.length === 0) {
-    const defaultSpeakers = [
-      { name: "Severus Snape", biography: "Potion Master" },
-      { name: "Harry Potter", biography: "Youngest seeker of the century" },
-      { name: "Hermione Granger", biography: "Excellent student" },
-    ];
-
-    for (const speaker of defaultSpeakers) {
-      const result = await createSpeaker(speaker);
-      if (result) {
-        console.log(`Speaker created: ${speaker.name}`);
-      }
-    }
-  }
-};
 
 export const fetchAndSetSpeakers = async (
   setSpeakers: (speakers: Speaker[]) => void
